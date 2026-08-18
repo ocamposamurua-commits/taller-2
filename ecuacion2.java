a161c00dc303dcd41c70b0ecaca4a3996b927394
David Robinson <drobinsonhernandez@gmail.com>
2026-08-17 23:22:16 -0500

    solicitar valores de x y z por teclado en ecuacion2.java

--- ecuacion2.java
+++ ecuacion2.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class ecuacion2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("--- CÁLCULO DE LA ECUACIÓN 2 ---");
+        //commit 6
+        System.out.print("Ingrese el valor de x: ");
+        double x = scanner.nextDouble();
+        System.out.print("Ingrese el valor de z: ");
+        double z = scanner.nextDouble();
 
-        // Aquí ingresaremos los datos y la fórmula en los siguientes pasos
+        // En el siguiente paso agregaremos la fórmula completa
 
         scanner.close();
     }
