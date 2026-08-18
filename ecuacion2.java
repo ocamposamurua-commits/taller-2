dc21c24e5e77c39ef465ce0b29caf34d9031897c
David Robinson <drobinsonhernandez@gmail.com>
2026-08-17 23:19:11 -0500

    crear archivo base ecuacion2.java con entrada Scanner

---
+++ ecuacion2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ecuacion2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- CÁLCULO DE LA ECUACIÓN 2 ---");
+
+        // Aquí ingresaremos los datos y la fórmula en los siguientes pasos
+
+        scanner.close();
+    }
+}
