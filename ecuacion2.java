1e1d5fb2a258f2e0e18f4ad519627020f8570fdc
David Robinson <drobinsonhernandez@gmail.com>
2026-08-17 23:26:45 -0500

    imprimir el resultado de y en la consola

--- ecuacion2.java
+++ ecuacion2.java
@@ -10,8 +10,22 @@ public class ecuacion2 {
         System.out.print("Ingrese el valor de z: ");
         double z = scanner.nextDouble();
 
-        // En el siguiente paso agregaremos la fórmula completa
+        //commit 7
 
+        // Término 1
+        double termino1 = (Math.pow(x, 2) + 3 * z + 2) / (2.0 + (1.0 / (1.0 + 2 * z)));
+
+        // Término 2 (Fracción compleja)
+        double numTermino2 = 2.0 / (1.0 + 3 * Math.pow(x, 3) + 3 * z + 2.0);
+        double denomFraccionInterna = 2 * z + (3.0 / (1.0 + (5.0 / z)));
+        double denTermino2 = 1.0 / ((1.0 / (1.0 + 3 * x)) + ((3 * x + 1) / denomFraccionInterna));
+
+        double termino2 = numTermino2 / denTermino2;
+
+        // Resultado final
+        double y = termino1 + termino2;
+        System.out.println("\nEl resultado de y es: " + y);
+        
         scanner.close();
     }
 }
