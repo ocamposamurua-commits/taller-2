f146046d43e871a5ee96678371c5b98ec15f93fe
Samuel <ocamposamurua@gmail.com>
2026-08-17 19:04:18 -0500

    Documentar tipos de variables

--- variables.java
+++ variables.java
@@ -6,24 +6,30 @@ public class Variables {
         int cantidad = 10;
         int numero = 25;
 
+        //Variables de tipo double
         double precio = 2.000;
         double altura = 1.80;
-        double hora = 2.30;
-
+        double hora = 4.50;
+        
+        //Variable de tipo float
         float temperatura = 36.5f;
         float peso = 61.0f;
         float distancia = 5.7f;
 
+        //Variables de tipo long
         long poblacion = 52000000L;
         long dinero = 1500000000L;
 
+        //Variables de tipo boolean
         boolean estudiante = true;
         boolean profesor = false;
 
+        //Variables de tipo char
         char inicial = 'S';
         char genero = 'M';
         char grado = 'A';
 
+        //Variables de tipo string
         String nombre = "Samuel";
         String ciudad = "Medellin";
         String carrera = "Ingenieria de Sistemas e Informatica";
